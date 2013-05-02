360e62df7644464405c878911431f606934a958b
Vsevolod Ivanov <seva.ask@gmail.com>
2013-05-02 18:11:43 +0400

    semantic vectors indexing works too

--- src/ru/uiiiii/ssearchm/Indexer.java
+++ src/ru/uiiiii/ssearchm/Indexer.java
@@ -1,26 +1,12 @@
 package ru.uiiiii.ssearchm;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Date;
 
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.NumericField;
 import org.apache.lucene.index.CorruptIndexException;
-import org.apache.lucene.index.FieldInfo.IndexOptions;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.index.IndexWriterConfig.OpenMode;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.LockObtainFailedException;
-import org.apache.lucene.util.Version;
+
+import pitt.search.semanticvectors.BuildIndex;
 
 public class Indexer {
 	
@@ -30,56 +16,39 @@ public class Indexer {
 	    String docsPath = DOCS_PATH;
 	    String indexPath = "index";
 	    
-	    final File docDir = new File(docsPath);
+	    buildLuceneIndex(docsPath, indexPath);
 	    
-	    Date start = new Date();
-	    System.out.println("Indexing to directory '" + indexPath + "'...");
+	    buildSemanticVectorsIndex(docsPath, indexPath);
+	}
 
-      	Directory dir = FSDirectory.open(new File(indexPath));
-      	Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_31);
-      	IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_31, analyzer);
+	private static void buildLuceneIndex(String docsPath, String indexPath)
+			throws IOException, CorruptIndexException,
+			LockObtainFailedException {
+		System.out.println("Lucene indexing...");
+	    
+	    Date startLuceneIndexing = new Date();
+	    
+	    LuceneIndexBuilder.buildIndex(docsPath, indexPath);
 
-        iwc.setOpenMode(OpenMode.CREATE);
-
-        IndexWriter writer = new IndexWriter(dir, iwc);
-        
-        indexDocsRecursive(writer, docDir);        
-
-        writer.close();
-
-	    Date end = new Date();
-	    System.out.println(end.getTime() - start.getTime() + " total milliseconds");
+	    Date endLuceneIndexing = new Date();
+	    System.out.println(endLuceneIndexing.getTime() - startLuceneIndexing.getTime() + " total milliseconds");
 	}
 	
-	private static void indexDocsRecursive(IndexWriter writer, File file) throws CorruptIndexException, IOException {
-		if (file.isDirectory()) {
-	        String[] files = file.list();
-	        for (String child : files) {
-	        	indexDocsRecursive(writer, new File(file, child));
-	        }
-		}
-		else {
-			indexDoc(writer, file);
-		}
-	}
-	
-	private static void indexDoc(IndexWriter writer, File file) throws CorruptIndexException, IOException {
-		FileInputStream fis = new FileInputStream(file);
+	private static void buildSemanticVectorsIndex(String docsPath,
+			String indexPath) throws IOException, CorruptIndexException,
+			LockObtainFailedException {
+		System.out.println("Semantic vectors indexing to directory '" + indexPath + "'...");
+	    
+		Date startSemanticVectorsIndexing = new Date();
 		
-        Document doc = new Document();
-        Field pathField = new Field("path", file.getPath(), Field.Store.YES, Field.Index.NOT_ANALYZED_NO_NORMS);
-        pathField.setIndexOptions(IndexOptions.DOCS_ONLY);
-        doc.add(pathField);
-        
-        NumericField modifiedField = new NumericField("modified");
-        modifiedField.setLongValue(file.lastModified());
-        doc.add(modifiedField);
-        
-        doc.add(new Field("contents", new BufferedReader(new InputStreamReader(fis, "UTF-8"))));
-
-        System.out.println("adding " + file);
-        writer.addDocument(doc);
-        
-        fis.close();
+		String[] args = new String[2];
+		String argumentIndexPath = "-luceneindexpath";
+		args[0] = argumentIndexPath;
+		String argumentIndexPath2 = "index";
+		args[1] = argumentIndexPath2;
+		BuildIndex.main(args);
+		
+	    Date endSemanticVectorsIndexing = new Date();
+	    System.out.println(endSemanticVectorsIndexing.getTime() - startSemanticVectorsIndexing.getTime() + " total milliseconds");
 	}
 }
---
+++ src/ru/uiiiii/ssearchm/LuceneIndexBuilder.java
@@ -0,0 +1,71 @@
+package ru.uiiiii.ssearchm;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.NumericField;
+import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.FieldInfo.IndexOptions;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.LockObtainFailedException;
+import org.apache.lucene.util.Version;
+
+public class LuceneIndexBuilder {
+	
+	public static void buildIndex(String docsPath, String indexPath) throws IOException, CorruptIndexException, LockObtainFailedException {
+		final File docDir = new File(docsPath);
+
+      	Directory dir = FSDirectory.open(new File(indexPath));
+      	Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_31);
+      	IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_31, analyzer);
+
+        iwc.setOpenMode(OpenMode.CREATE);
+
+        IndexWriter writer = new IndexWriter(dir, iwc);
+        luceneIndexDocsRecursive(writer, docDir);
+        writer.close();
+	}
+	
+	private static void luceneIndexDocsRecursive(IndexWriter writer, File file) throws CorruptIndexException, IOException {
+		if (file.isDirectory()) {
+	        String[] files = file.list();
+	        for (String child : files) {
+	        	luceneIndexDocsRecursive(writer, new File(file, child));
+	        }
+		}
+		else {
+			luceneIndexDoc(writer, file);
+		}
+	}
+	
+	private static void luceneIndexDoc(IndexWriter writer, File file) throws CorruptIndexException, IOException {
+		FileInputStream fis = new FileInputStream(file);
+		
+        Document doc = new Document();
+        Field pathField = new Field("path", file.getPath(), Field.Store.YES, Field.Index.NOT_ANALYZED_NO_NORMS);
+        pathField.setIndexOptions(IndexOptions.DOCS_ONLY);
+        doc.add(pathField);
+        
+        NumericField modifiedField = new NumericField("modified");
+        modifiedField.setLongValue(file.lastModified());
+        doc.add(modifiedField);
+        
+        doc.add(new Field("contents", new BufferedReader(new InputStreamReader(fis, "UTF-8"))));
+
+        System.out.println("adding " + file);
+        writer.addDocument(doc);
+        
+        fis.close();
+	}
+}
