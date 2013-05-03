99fb00c6d6561c6ff7eb018830e2dcc2c5af97cb
Vsevolod Ivanov <seva.ask@gmail.com>
2013-05-03 20:29:12 +0400

    git integrating

--- src/ru/uiiiii/ssearchm/searching/Searcher.java
+++ src/ru/uiiiii/ssearchm/searching/Searcher.java
@@ -2,25 +2,44 @@ package ru.uiiiii.ssearchm.searching;
 
 import java.io.IOException;
 import java.util.LinkedList;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.NoHeadException;
+import org.eclipse.jgit.revwalk.RevCommit;
 
 import pitt.search.semanticvectors.SearchResult;
 import pitt.search.semanticvectors.ZeroVectorException;
+import ru.uiiiii.ssearchm.indexing.Indexer;
 
 public class Searcher {
 
 	private static final int MAX_RESULTS = 20;
 
-	public static void main(String[] args) throws IOException, ZeroVectorException {
+	public static void main(String[] args) throws IOException, ZeroVectorException, NoHeadException, GitAPIException {
 		String[] query = {"queue"};
 		
 		LinkedList<SearchResult> results = SemanticVectorsSearcher.performSearch(query, MAX_RESULTS);
 		
+		GitHelper gitHelper = new GitHelper(Indexer.DOCS_PATH);
+		
+		TreeSet<RevCommit> commits = new TreeSet<RevCommit>();
+		
 		for (SearchResult result: results) {
-			  System.out.println(String.format(
-			      "%f:%s",
-			      result.getScore(),
-			      result.getObjectVector().getObject().toString()));
+			String filePath = result.getObjectVector().getObject().toString();
+			Set<RevCommit> fileCommits = gitHelper.getCommitsFromFile(filePath);
+			commits.addAll(fileCommits);	
 		}
+		
+		System.out.println(commits.size());
+		
+//		for (SearchResult result: results) {
+//			  System.out.println(String.format(
+//			      "%f:%s",
+//			      result.getScore(),
+//			      result.getObjectVector().getObject().toString()));
+//		}
 	}
 
 }
