fc6184f3d1a73ca26a9567c15f26e55e1b6d076b
Vsevolod Ivanov <seva.ask@gmail.com>
2013-05-05 14:30:28 +0400

    small path fix

--- src/ru/uiiiii/ssearchm/common/SourceData.java
+++ src/ru/uiiiii/ssearchm/common/SourceData.java
@@ -2,7 +2,7 @@ package ru.uiiiii.ssearchm.common;
 
 public class SourceData {
 	
-	public final static String DOCS_PATH = "C:\\Projects\\SSearchM\\tests\\hudson";
+	public final static String DOCS_PATH = "C:\\Projects\\SSearchM\\tests\\1\\hudson";
 	
 	public final static String[] QUERY = {"queue"};
 	
